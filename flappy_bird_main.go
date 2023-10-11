ef9aa9b98e772abfae53b8f6165bfc6044ea82d4
Julian Freund <julian.freund@AD.B-SZ-WSGY.LOGODIDACT.NET>
2023-10-11 08:52:11 +0200

    Datentypen geaendert, nur noch int

--- flappy_bird_main.go
+++ flappy_bird_main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
     "gfx2"
     "time"
-    "math"
+    //"math"
     "image"
 	"imaging"
 	"os"
@@ -14,21 +14,22 @@ import (
 //Main
 func main(){
 	// Variablen erstellen
-	var speed float64 = 0
-	var birdposX float64 = 100
-	var birdposY float64 = 100
-	var timeInterval float64 = 0.1
-	var acceleration float64 = -20
+	var speed int = 0
+	var birdposX int = 100
+	var birdposY int = 100
+	var timeInterval int = 10
+	var acceleration int = -20
 	var click int
 	ch := make(chan int)
-	var windowX uint16 = 1000
-	var windowY uint16 = 800
-
+	var windowX uint16 = 1000 //Wird nur fürs Fenster genutzt (Fenster nimmt x,y als uint16)
+	var windowY int = 800 //Wird unter Anderem für Scale Funktion genutzt, deswegen int 
+	var height int
+	
 	//Scale Images
-	scale_Image(float64(windowY))
+	height = scale_Image(windowY)
 		
 	// Erzeuge Fenster
-	gfx2.Fenster(windowX, windowY)
+	gfx2.Fenster(windowX, uint16(windowY))
 	gfx2.Fenstertitel ("Flappy Bird")
 	
 	//UpdateAus() um Vogel-Fehler zu vermeiden
@@ -41,8 +42,9 @@ func main(){
 	// Vogel reinladen
 	// gfx2.LadeBildMitColorKey(birdposx, birdposy, "Frame-1.bmp",0,0,0)
 	
-	gfx2.LadeBildInsClipboard("Frame-1.bmp")
-	gfx2.Clipboard_einfuegenMitColorKey (uint16(birdposX), uint16(birdposY), 255,0,0) 
+	//Clipboard_einfuegenMitColorKey nimmt y-Wert als uint16
+	gfx2.LadeBildInsClipboard("./images/Frame-1.bmp")
+	gfx2.Clipboard_einfuegenMitColorKey(uint16(birdposX), uint16(birdposY), 255,0,0) 
 	
 	gfx2.UpdateAn()
 	
@@ -58,26 +60,25 @@ func main(){
 				if click == 1 {
 					
 				speed = 40
-				birdposY -= float64(speed)
+				birdposY -= speed
 			
 				}
 			default:
-				speed += timeInterval * acceleration
-				birdposY -= float64(speed * timeInterval)
+				speed += acceleration / timeInterval 
+				birdposY -= speed / timeInterval
 				
 		
         // Clear the screen
         gfx2.UpdateAus()
         gfx2.Cls()
 			
-				
-        if birdposY < 0 {
+		if birdposY < 0 {
 			birdposY = 0
-			
-		}else if birdposY > 630 {
-			birdposY = 630
-		}
 		
+		} else if birdposY > (windowY-height) {
+			birdposY = windowY - height
+		}
+
         // Draw the sprite at its new position
         gfx2.LadeBild(uint16(birdposX), uint16(birdposY), "./images/Frame-1.bmp")
 
@@ -114,10 +115,22 @@ func Mauslesen(ch chan int){
 	}
 }
 
-func scale_Image(windowY float64){
+func scale_Image(windowY int) int{
 	
 		image_list := []string{"./Frame-1.bmp", "./Frame-2.bmp", "./Frame-3.bmp", "./Frame-4.bmp"}
 		rescaled_image_list := []string{"./images/Frame-1.bmp", "./images/Frame-2.bmp", "./images/Frame-3.bmp", "./images/Frame-4.bmp"}
+		
+		//Verhältnis WindowY <--> BirdX = 0,25 == 25 / 100 (um float zu vermeiden)
+		//Verhältnis WindowY <--> BirdY = 0,225 == 225 / 1000 (um float zu vermeiden)
+		
+		//Festlegen der neuen Größe des Vogels:
+			
+		var width int = (windowY * 25) / 100 
+		var height int = (windowY * 225) / 1000
+		
+		//Ausgeben zum Debuggen
+		fmt.Println(width)
+		fmt.Println(height)
 		
 		//Loop zum scalen aller Bilder
 		for i:=0; i < 4; i++ {
@@ -138,14 +151,6 @@ func scale_Image(windowY float64){
 				defer file.Close()
 				}
 				
-			//Verhältnis WindowY <--> BirdX = 0,25 
-			//Verhältnis WindowY <--> BirdY = 0,225
-
-			//Festlegen der neuen Größe des Vogels:
-			
-			var width int = int(math.Round(windowY * 0.25))
-			var height int = int(math.Round(windowY * 0.225))
-			
 			//Bildgröße verändern
 			resizedImage := imaging.Resize(img, width, height, imaging.NearestNeighbor)
 			
@@ -153,9 +158,9 @@ func scale_Image(windowY float64){
 			err = imaging.Save(resizedImage, rescaled_image_list[i])
 			if err != nil{
 				fmt.Println("Speichern der Datei fehlgeschlagen:", err)
-				return
 				}		
 			}
+		return height
 }
 
 
