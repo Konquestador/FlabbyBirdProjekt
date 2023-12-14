5fb4593289c4a00e561d6a7be7f739c295e9b72e
Alexander Kelm <alexander.kelm@AD.B-SZ-WSGY.LOGODIDACT.NET>
2023-12-14 14:14:37 +0100

    Säulen kommentiert

--- saeulen/saeulenimpl.go
+++ saeulen/saeulenimpl.go
@@ -1,114 +1,129 @@
 package saeulen
 
 import (
-    "gfx2"
-    "fmt"
-    "zufallszahlen"
+	"gfx2"
+	"fmt"
+	"zufallszahlen"
 )
 
-type data struct{
-	xWert uint16
-	breite uint16
-	rXWert int
-	hoehe uint16
-	loch uint16
-	geschwindigkeit uint16
-	passed bool
-	touch bool
-	birdInHole bool
+// Saeule repräsentiert eine Struktur zur Verwaltung von Säulen im Spiel.
+type Saeule struct {
+	xWert          uint16 // X-Koordinate der Säule
+	breite         uint16 // Breite der Säule
+	rXWert         int    // Relative X-Koordinate für die Bewegung
+	hoehe          uint16 // Höhe der Säule
+	loch           uint16 // Höhe der Lücke in der Säule
+	geschwindigkeit uint16 // Geschwindigkeit der Säule
+	passed         bool   // Flag, ob der Spieler die Säule passiert hat
+	touch          bool   // Flag, ob die Säule berührt wurde
+	birdInHole     bool   // Flag, ob der Vogel in der Lücke der Säule ist
 }
 
-func New () *data {
-	var s *data
-	s = new(data)
-	s.breite=100
-	s.passed= false
+// New erstellt eine neue Säule mit Standardwerten.
+func New() *Saeule {
+	var s *Saeule
+	s = new(Saeule)
+	s.breite = 100
+	s.passed = false
 	return s
 }
 
-func (s *data) SetzeZufallswerte() {
+// SetzeZufallswerte setzt zufällige Werte für die x-Koordinate, Höhe und Lückenhöhe der Säule.
+func (s *Saeule) SetzeZufallswerte() {
 	zufallszahlen.Randomisieren()
 	s.xWert = gfx2.Grafikspalten()
-	s.hoehe = uint16(zufallszahlen.Zufallszahl(50,500))
-	s.loch = uint16(zufallszahlen.Zufallszahl(200,300))
+	s.hoehe = uint16(zufallszahlen.Zufallszahl(50, 500))
+	s.loch = uint16(zufallszahlen.Zufallszahl(200, 300))
 }
 
-func (s *data) GibXWert()  uint16{
+// GibXWert gibt die x-Koordinate der Säule zurück.
+func (s *Saeule) GibXWert() uint16 {
 	return s.xWert
 }
 
-func (s * data) GibPassed() bool{
+// GibPassed gibt zurück, ob der Spieler die Säule passiert hat.
+func (s *Saeule) GibPassed() bool {
 	return s.passed
-	}
+}
 
-func (s *data) GibBreite() uint16 {
+// GibBreite gibt die Breite der Säule zurück.
+func (s *Saeule) GibBreite() uint16 {
 	return s.breite
 }
 
-func (s *data) GibHoehe() uint16 {
+// GibHoehe gibt die Höhe der Säule zurück.
+func (s *Saeule) GibHoehe() uint16 {
 	return s.hoehe
 }
 
-func (s *data) GibLoch() uint16 {
+// GibLoch gibt die Höhe der Lücke in der Säule zurück.
+func (s *Saeule) GibLoch() uint16 {
 	return s.loch
 }
 
-func (s * data) GibbirdInHole() bool{
+// GibbirdInHole gibt zurück, ob der Vogel in der Lücke der Säule ist.
+func (s *Saeule) GibbirdInHole() bool {
 	return s.birdInHole
-	}
+}
 
-func (s * data) InHole(){
+// InHole setzt das Flag birdInHole auf true.
+func (s *Saeule) InHole() {
 	s.birdInHole = true
 }
 
-func (s * data) NotInHole(){
+// NotInHole setzt das Flag birdInHole auf false.
+func (s *Saeule) NotInHole() {
 	s.birdInHole = false
 }
 
-func (s * data) Passed(){
+// Passed setzt das Flag passed auf true.
+func (s *Saeule) Passed() {
 	s.passed = true
 }
 
-func (s * data) Touch(){
+// Touch setzt das Flag touch auf true.
+func (s *Saeule) Touch() {
 	s.touch = true
 }
 
-func (s *data) SetzeWerte(xWert,hoehe,loch uint16) {
+// SetzeWerte setzt die Werte der x-Koordinate, Höhe und Lückenhöhe für die Säule.
+func (s *Saeule) SetzeWerte(xWert, hoehe, loch uint16) {
 	s.xWert = xWert
 	s.hoehe = hoehe
 	s.loch = loch
-}  
-
-func (s *data) String () string {
-	var erg string
-	erg = fmt.Sprintln(s.xWert,s.hoehe,s.loch)
-	return erg
 }
 
-func (s *data) Move () {
+// String gibt eine Zeichenkettenrepräsentation der Werte der Säule zurück.
+func (s *Saeule) String() string {
+	return fmt.Sprintln(s.xWert, s.hoehe, s.loch)
+}
+
+// Move aktualisiert die Position der Säule basierend auf ihrer Geschwindigkeit.
+func (s *Saeule) Move() {
 	s.geschwindigkeit = 5
-	nx:=int(s.xWert)-int(s.geschwindigkeit)+s.rXWert
-	if nx>0 {
+	nx := int(s.xWert) - int(s.geschwindigkeit) + s.rXWert
+	if nx > 0 {
 		s.xWert = s.xWert - s.geschwindigkeit
-	}else if nx < (-100) {
+	} else if nx < -100 {
 		s.xWert = 20000
-	}else{
-		s.xWert=0
-		s.breite=100-uint16(-1*nx)
+	} else {
+		s.xWert = 0
+		s.breite = 100 - uint16(-1*nx)
 		s.rXWert = nx
-	} 
+	}
 }
 
-func (s *data) Draw(birdposX int, width int) {
+// Draw erstellt die Säule im Grafikfenster.
+func (s *Saeule) Draw(birdposX int, width int) {
 	var fensterhoehe uint16
 	if gfx2.FensterOffen() {
 		fensterhoehe = gfx2.Grafikzeilen()
 	}
-	if s.touch{
-		gfx2.Vollrechteck(uint16(birdposX + width + 1),0,s.breite,s.hoehe)
-		gfx2.Vollrechteck(uint16(birdposX + width + 1),s.hoehe+s.loch,s.breite,fensterhoehe-(s.hoehe+s.loch))
-	}else{
-		gfx2.Vollrechteck(s.xWert,0,s.breite,s.hoehe)
-		gfx2.Vollrechteck(s.xWert,s.hoehe+s.loch,s.breite,fensterhoehe-(s.hoehe+s.loch))
+	if s.touch {
+		gfx2.Vollrechteck(uint16(birdposX+width+1), 0, s.breite, s.hoehe)
+		gfx2.Vollrechteck(uint16(birdposX+width+1), s.hoehe+s.loch, s.breite, fensterhoehe-(s.hoehe+s.loch))
+	} else {
+		gfx2.Vollrechteck(s.xWert, 0, s.breite, s.hoehe)
+		gfx2.Vollrechteck(s.xWert, s.hoehe+s.loch, s.breite, fensterhoehe-(s.hoehe+s.loch))
 	}
 }
